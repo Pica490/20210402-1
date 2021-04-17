de7b8dbba0fc1feb3eaef29c20f6ddbc25e7c409
Pica490 <m_kuznecova@rambler.ru>
2021-04-17 13:41:35 +0500

    Information print added

--- main.py
+++ main.py
@@ -15,6 +15,10 @@ class Student:
                 lecturer.grades1[course] = [grade]
         else:
             return 'Ошибка'
+    def __str__(self):
+        a = some_name.grades.values()
+        return f'Имя: {self.name}\nФамилия: {self.surname}\nСредняя оценка за домашнее задание:{average(a):.1f}\n' \
+               f'Завершенные курсы:{self.finished_courses}\nКурсы в процессе изучения:{self.courses_in_progress}'
 
 class Mentor:
     def __init__(self, name, surname):
@@ -36,27 +40,55 @@ class Lecturer(Mentor):
         super().__init__(name, surname)
         self.grades1 = {}
 
+    def __str__(self):
+        a = some_name.grades1.values()
+        return f'Имя: {self.name}\nФамилия: {self.surname}\nСредняя оценка за лекции:{average(a):.1f}\n'
+
+def average(a):
+    sum = 0
+    count = 0
+    for value in list(a):
+        for number in value:
+           sum = sum + number
+           count = count + 1
+    return sum / count
+
+def count_1(a):
+    count = 0
+    for value in list(a):
+        for number in value:
+            count = count + 1
+    return count
+
 class Reviewer(Mentor):
-    pass
+    def __str__(self):
+        return f'\nИмя:{self.name}\nФамилия: {self.surname}\n'
+
 
 best_student = Student('Ruoy', 'Eman', 'your_gender')
-best_student.courses_in_progress += ['Python','Java']
+best_student.courses_in_progress += ['Python']
+best_student.finished_courses += ['Java', 'Git']
 
-cool_mentor = Mentor('Some', 'Buddy')
-cool_mentor.courses_attached += ['Python']
-
-Reviewer_1 = cool_mentor
+Reviewer_1 = Reviewer('Some','Buddy')
+Reviewer_1.courses_attached += ['Python']
 
 Reviewer_1.rate_hw(best_student, 'Python', 10)
 Reviewer_1.rate_hw(best_student, 'Python', 10)
 Reviewer_1.rate_hw(best_student, 'Python', 10)
 
 lecturer_1 = Lecturer ('Vasilisa','Intelligent')
-lecturer_1.courses_attached += ['Python','Java']
+lecturer_1.courses_attached += ['Python','Java','1']
 
 best_student.rate_l(lecturer_1, 'Python', 10)
+best_student.rate_l(lecturer_1, 'Python', 2)
 best_student.rate_l(lecturer_1, 'Java', 5)
 
-print (Reviewer_1.name)
-print (best_student.grades)
-print (lecturer_1.grades1)
+some_name = Reviewer_1
+print(some_name)
+
+some_name = lecturer_1
+print(some_name)
+
+some_name = best_student
+print(some_name)
+
